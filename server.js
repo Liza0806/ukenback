2372e2ded09f346027355d5647443fc6762223aa
Liza <fildepiorsta@gmail.com>
2024-08-23 19:50:04 +0300

    Update server.js for Vercel deployment

--- server.js
+++ server.js
@@ -2,16 +2,33 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const { DB_HOST } = require("./config");
 
-const PORT = 3201;
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3001");
-    });
-    console.log("Database connect success");
-  })
-  .catch((error) => {
-    console.log(error.message, "error");
-    process.exit(1);
-  });
+mongoose.connect(DB_HOST).then(() => {
+  console.log("Database connect success");
+}).catch((error) => {
+  console.log(error.message, "error");
+  process.exit(1);
+});
+
+module.exports = (req, res) => {
+  app(req, res);  // Передаем запрос и ответ в Express приложение
+};
+
+
+
+// const mongoose = require("mongoose");
+// const app = require("./app");
+// const { DB_HOST } = require("./config");
+
+// const PORT = 3201;
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log("Server running. Use our API on port: 3001");
+//     });
+//     console.log("Database connect success");
+//   })
+//   .catch((error) => {
+//     console.log(error.message, "error");
+//     process.exit(1);
+//   });
