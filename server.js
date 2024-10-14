5980e5e32c67acdbfc3c248807572c1dca01d17f
Liza <fildepiorsta@gmail.com>
2024-10-14 10:58:43 +0300

    new

--- server.js
+++ server.js
@@ -16,21 +16,3 @@ module.exports = (req, res) => {
 };
 
 
-
-// const mongoose = require("mongoose");
-// const app = require("./app");
-// const { DB_HOST } = require("./config");
-
-// const PORT = 3201;
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log("Server running. Use our API on port: 3001");
-//     });
-//     console.log("Database connect success");
-//   })
-//   .catch((error) => {
-//     console.log(error.message, "error");
-//     process.exit(1);
-//   });
