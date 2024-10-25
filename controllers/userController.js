20fef3db21ed29564ebacd02b1384ccf3385399e
Liza <fildepiorsta@gmail.com>
2024-10-25 19:02:51 +0300

    id

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ const getAllUsers = async (req, res) => {
 const getUserByUserId = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await User.findOne({ "_id": id });
+    const user = await User.findById(id);
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
